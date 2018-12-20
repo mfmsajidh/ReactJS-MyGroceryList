28b78e3c98ddce7c7aecaaeb0c52ddb886f2c6cb
mfmsajidh <mohamedsajidh.2018851@iit.ac.lk>
2018-12-20 15:03:31 +0530

    Added Dialog Popup for editing grocery list

--- src/components/BtnDelete.js
+++
@@ -1,9 +0,0 @@
-import {Button} from "@material-ui/core";
-import React from "react";
-import {app} from "./app"
-
-export function BtnDelete (props){
-    return (
-        <Button color="secondary" onClick={()=>app.Delete(props.index)}>Delete</Button>
-    )
-}
